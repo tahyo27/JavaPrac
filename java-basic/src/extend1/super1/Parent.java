b22e5a942681be0637932b9c608702ddd28dd7bf
tahyo27 <tahyo27@gmail.com>
2025-09-16 20:21:15 +0900

    basic add super1

---
+++ java-basic/src/extend1/super1/Parent.java
@@ -0,0 +1,8 @@
+package extend1.super1;
+
+public class Parent {
+    public String value = "parent";
+    public void hello() {
+        System.out.println("Parent.hello");
+    }
+}
---
+++ java-basic/src/extend1/super1/Super1Main.java
@@ -0,0 +1,8 @@
+package extend1.super1;
+
+public class Super1Main {
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.call();
+    }
+}
