9cc9b265bb5f68a87c493ec4a7ed652f292f0342
tahyo27 <tahyo27@gmail.com>
2025-11-11 20:25:56 +0900

    add poly0 carmodel

--- java-basic/src/poly/car0/Car0Main.java
+++ java-basic/src/poly/car0/Car0Main.java
@@ -6,5 +6,10 @@ public class Car0Main {
         K3Car k3Car = new K3Car();
         driver.setK3Car(k3Car);
         driver.drive();
+        //추가
+        Model3Car model3Car = new Model3Car();
+        driver.setK3Car(null);
+        driver.setModel3Car(model3Car);
+        driver.drive();
     }
 }
